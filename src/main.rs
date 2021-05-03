5a3aae7724939486c6295e46a2623f0884411885
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-05-04 00:00:29 +0900

    別の場所で共通のオブジェクト的なの使えるの確認した

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
-use actix_web::{get, post, put, delete, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, post, put, delete, App, HttpResponse, HttpServer, Responder};
+use kotoba_player::KotobaPlayer;
+use std::sync::Mutex;
+
+struct AppState {
+    kotoba_player: Mutex<KotobaPlayer>,
+}
 
 #[get("/")]
 async fn get() -> impl Responder {
     HttpResponse::Ok().body("get ok")
 }
-#[post("/")]
-async fn post() -> impl Responder {
-    HttpResponse::Ok().body("post ok")
-}
-#[put("/")]
-async fn put() -> impl Responder {
-    HttpResponse::Ok().body("put ok")
-}
-#[delete("/")]
-async fn delete() -> impl Responder {
-    HttpResponse::Ok().body("delete ok")
+
+#[get("/parrot")]
+async fn parrot(data: web::Data<AppState>) -> impl Responder {
+    let mut kotoba = data.kotoba_player.lock().unwrap();
+    HttpResponse::Ok().body(kotoba.parrot("そうきちゃんは可愛いです"))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let state = web::Data::new(AppState{
+        kotoba_player: Mutex::new(KotobaPlayer::new("/app/dic")),
+    });
+    HttpServer::new(move || {
         App::new()
-        .service(get)
-        .service(post)
-        .service(put)
-        .service(delete)
+            .app_data(state.clone())
+            .service(get)
+            .service(parrot)
     })
     .bind("0.0.0.0:8080")?
     .run()
