33b21678186d853a05801df11338452fb2b2427b
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-05-04 15:10:03 +0900

    名詞をmaskするやつ作った

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, delete, get, post, put, web};
-use kotoba_player::KotobaPlayer;
+use kotoba_player::{KotobaPlayer, Mask};
 use serde::{Serialize, Deserialize};
 use std::sync::Mutex;
 
@@ -10,6 +10,13 @@ struct AppState {
 #[derive(Serialize, Deserialize)]
 struct Text {
     text: String,
+}
+
+#[derive(Deserialize)]
+struct MaskRequestBody {
+    text: String,
+    mask_token: String,
+    char_by_char: bool,
 }
 
 #[get("/")]
@@ -24,6 +31,25 @@ async fn parrot((text, state): (web::Json<Text>, web::Data<AppState>)) -> impl R
     HttpResponse::Ok().json(body)
 }
 
+#[post("/masquerade")]
+async fn masquerade((req, state): (web::Json<MaskRequestBody>, web::Data<AppState>)) -> impl Responder {
+    let mut kotoba = state.kotoba_player.lock().unwrap();
+
+    let mask  = if req.char_by_char {
+        let c:Vec<char> = req.mask_token.chars().collect();
+        if c.len() == 1 {
+            Mask::CharByChar(c[0])
+        }else {
+            return HttpResponse::BadRequest().body("should mask token is one character when char by char masking");
+        }
+    } else {
+        Mask::WordByWord(req.mask_token.clone())
+    };
+
+    let body = Text{text: kotoba.masquerade(&req.text, mask)};
+    HttpResponse::Ok().json(body)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let state = web::Data::new(AppState{
@@ -34,6 +60,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(state.clone())
             .service(get)
             .service(parrot)
+            .service(masquerade)
     })
     .bind("0.0.0.0:8080")?
     .run()
